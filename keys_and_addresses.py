084b2fa165203057b691beb2d44459780f53326e
sandot001 <sandyvirgo88@gmail.com>
2025-01-23 14:14:42 +0700

    Add files via upload

---
+++ keys_and_addresses.py
@@ -0,0 +1,13 @@
+# keys_and_addresses.py
+# replace you key and address 
+private_keys = [
+    'your_private_key_here'  # Example: '9da...365' 
+]
+
+my_addresses = [
+    'your_address_here'  # Example: '0x087....E85' 
+]
+
+labels = [
+    'wallet1' 
+]
